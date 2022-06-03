4322d31b83869c9714e68eb0550b5fcf2b0edbcf
Zubair Aslam <zubul93@yahoo.com>
2022-06-03 10:03:40 -0500

    Initial commit

---
+++ Validator4/src/main/java/com/hexaware/Validator4/Validator4Application.java
@@ -0,0 +1,13 @@
+package com.hexaware.Validator4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Validator4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Validator4Application.class, args);
+	}
+
+}
---
+++ Validator4/src/test/java/com/hexaware/Validator4/Validator4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexaware.Validator4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Validator4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
